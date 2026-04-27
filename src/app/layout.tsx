6c81c3fa8a3849df2938497807dcaef780fd8180
adgrossm <adgrossm@gmail.com>
2026-04-27 12:41:57 -0400

    add google analytics GA4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,7 +42,23 @@ export default function RootLayout({
                 c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                 t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                 y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-              })(window, document, "clarity", "script", "YOUR_PROJECT_ID");
+              })(window, document, "clarity", "script", "wib61rym4u");
+            `,
+          }}
+        />
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-JKZ4Q7R8F9"
+          strategy="afterInteractive"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-JKZ4Q7R8F9');
             `,
           }}
         />
